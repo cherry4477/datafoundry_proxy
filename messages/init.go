73801478f27700a7702900f6167321ff4f6221de
yaxinlx <liuxu@asiainfo.com>
2016-08-08 11:30:55 +0800

    remove mq references

--- messages/init.go
+++ messages/init.go
@@ -5,17 +5,15 @@ import (
 	"fmt"
 	//"os"
 	"time"
-	"net"
 
 	//"github.com/julienschmidt/httprouter"
 
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/asiainfoLDP/datahub_commons/log"
-	"github.com/asiainfoLDP/datahub_commons/mq"
-	
+
 	"github.com/asiainfoLDP/datafoundry_proxy/env"
-	
+
 	"github.com/asiainfoLDP/datafoundry_proxy/messages/notification"
 )
 
@@ -29,11 +27,13 @@ var Logger = log.DefaultlLogger()
 
 var mysqlEnv, kafkaEnv, emailEnv env.Env
 
-func Init(/*router *httprouter.Router, */_mysqlEnv, _kafkaEnv, _emailEnv env.Env) bool {
-	
+func Init( /*router *httprouter.Router, */ _mysqlEnv, _kafkaEnv, _emailEnv env.Env) bool {
+
 	mysqlEnv, kafkaEnv, emailEnv = _mysqlEnv, _kafkaEnv, _emailEnv
-	
-	if initDB() == false {return false}
+
+	if initDB() == false {
+		return false
+	}
 
 	//initRouter(router)
 	//initGateWay()
@@ -63,11 +63,11 @@ func MysqlAddrPort() (string, string) {
 }
 
 func MysqlDatabaseUsernamePassword() (string, string, string) {
-	//return os.Getenv(os.Getenv("ENV_NAME_MYSQL_DATABASE")), 
-	//	os.Getenv(os.Getenv("ENV_NAME_MYSQL_USER")), 
+	//return os.Getenv(os.Getenv("ENV_NAME_MYSQL_DATABASE")),
+	//	os.Getenv(os.Getenv("ENV_NAME_MYSQL_USER")),
 	//	os.Getenv(os.Getenv("ENV_NAME_MYSQL_PASSWORD"))
-	return mysqlEnv.Get("ENV_NAME_MYSQL_DATABASE"), 
-		mysqlEnv.Get("ENV_NAME_MYSQL_USER"), 
+	return mysqlEnv.Get("ENV_NAME_MYSQL_DATABASE"),
+		mysqlEnv.Get("ENV_NAME_MYSQL_USER"),
 		mysqlEnv.Get("ENV_NAME_MYSQL_PASSWORD")
 }
 
@@ -107,8 +107,8 @@ func initDB() bool {
 	upgradeDB()
 
 	go updateDB()
-	
-	return  true
+
+	return true
 }
 
 func updateDB() {
@@ -128,7 +128,7 @@ func updateDB() {
 func connectDB() {
 	DB_ADDR, DB_PORT := MysqlAddrPort()
 	DB_DATABASE, DB_USER, DB_PASSWORD := MysqlDatabaseUsernamePassword()
-	
+
 	DB_URL := fmt.Sprintf(`%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=true`, DB_USER, DB_PASSWORD, DB_ADDR, DB_PORT, DB_DATABASE)
 
 	Logger.Info("connect to ", DB_URL)
@@ -136,7 +136,7 @@ func connectDB() {
 	if err == nil {
 		err = db.Ping()
 	}
-	
+
 	if err != nil {
 		Logger.Errorf("error: %s\n", err)
 	} else {
@@ -156,7 +156,7 @@ func upgradeDB() {
 /*
 var (
 	ApiGateway string
-	
+
 	RepositoryService   string
 	SubscriptionSercice string
 	UserService         string
@@ -179,7 +179,7 @@ func BuildServiceUrlPrefixFromEnv(name string, addrEnv string, portEnv string) s
 	}
 
 	Logger.Infof("%s = %s", name, prefix)
-	
+
 	return prefix
 }
 
@@ -212,128 +212,3 @@ func getDeamonService() string {
 	return DeamonService
 }
 */
-
-var (
-	theMQ mq.MessageQueue
-)
-
-func KafkaAddrPort() (string, string) {
-	//return os.Getenv(os.Getenv("ENV_NAME_KAFKA_ADDR")), os.Getenv(os.Getenv("ENV_NAME_KAFKA_PORT"))
-	return kafkaEnv.Get("ENV_NAME_KAFKA_ADDR"), kafkaEnv.Get("ENV_NAME_KAFKA_PORT")
-}
-
-func initMQ() {
-	connectMQ()
-	go updateMQ()
-}
-
-func updateMQ() {
-	var err error
-	ticker := time.Tick(5 * time.Second)
-	for _ = range ticker {
-		if theMQ == nil {
-			connectMQ()
-		} else if _, _, err = theMQ.SendSyncMessage("__ping__", []byte(""), []byte("")); err != nil {
-			theMQ.Close()
-			//theMQ = nil // draw snake feet
-			connectMQ()
-		}
-	}
-}
-
-func connectMQ() {
-	kafkas := net.JoinHostPort(KafkaAddrPort())
-	Logger.Infof("kafkas = %s", kafkas)
-	var err error
-	theMQ, err = mq.NewMQ([]string{kafkas}) // ex. {"192.168.1.1:9092", "192.168.1.2:9092"}
-	if err != nil {
-		Logger.Infof("initMQ error: %s", err.Error())
-		return
-	}
-	
-	// ...
-
-	notificationListener := newNotificationListener("handler")
-
-	theMQ.SendSyncMessage("to_notifications.json", []byte(""), []byte("")) // force create the topic
-	// 0 is the partition id
-	err = theMQ.SetMessageListener("to_notifications.json", 0, mq.Offset_Marked, notificationListener)
-	if err != nil {
-		Logger.Info("SetMessageListener (to_notifications.json) error: ", err)
-	}
-	
-	// ...
-
-	emailListener := newEmailListener("handler")
-
-	theMQ.SendSyncMessage("to_emails.json", []byte(""), []byte("")) // force create the topic
-	// 0 is the partition id
-	err = theMQ.SetMessageListener("to_emails.json", 0, mq.Offset_Marked, emailListener)
-	if err != nil {
-		Logger.Info("SetMessageListener (to_emails.json) error: ", err)
-	}
-}
-
-//func pushSyncMessageIntoQueue(topic string, key, value []string) error {
-//	return nil
-//}
-//
-//func pushAsyncMessageIntoQueue(topic string, key, value []string) error {
-//	return nil
-//}
-
-//---------------------------
-
-type NotificationListener struct {
-	name string
-}
-
-func newNotificationListener(name string) *NotificationListener {
-	return &NotificationListener{name: name}
-}
-
-func (listener *NotificationListener) OnMessage(topic string, partition int32, offset int64, key, value []byte) bool {
-	//log.Debugf("%s received: (%d) message: %s", listener.name, offset, string(value))
-	err := HandleNotificationsFromQueue(topic, key, value)
-	if err != nil {
-		Logger.Errorf("NotificationListener OnMessage(%s:%d:%d, %s=%s) error: %s", topic, partition, offset, string(key), string(value), err.Error())
-		// return false // no a good idea to return false
-	} else {
-		Logger.Debugf("NotificationListener OnMessage succeded. %s=%s", string(key), string(value))
-	}
-
-	return true
-}
-
-func (listener *NotificationListener) OnError(err error) bool {
-	Logger.Debugf("NotificationListener OnError: %s", err.Error())
-	return false
-}
-
-//---------------------------
-
-type EmailListener struct {
-	name string
-}
-
-func newEmailListener(name string) *EmailListener {
-	return &EmailListener{name: name}
-}
-
-func (listener *EmailListener) OnMessage(topic string, partition int32, offset int64, key, value []byte) bool {
-	//log.Debugf("%s received: (%d) message: %s", listener.name, offset, string(value))
-	err := HandleEmailsFromQueue(topic, key, value)
-	if err != nil {
-		Logger.Warningf("EmailListener OnMessage(%s:%d:%d, %s=%s) error: %s", topic, partition, offset, string(key), string(value), err.Error())
-		// return false // no a good idea to return false
-	} else {
-		Logger.Debugf("EmailListener OnMessage succeded. %s=%s", string(key), string(value))
-	}
-
-	return true
-}
-
-func (listener *EmailListener) OnError(err error) bool {
-	Logger.Debugf("EmailListener OnError: %s", err.Error())
-	return false
-}
